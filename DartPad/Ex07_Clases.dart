e437c9e8f7b04bc6620a2e3cc3f3ad51f4792fa4
Janeth-Ahuacatitla <210084@utxicotepec.edu.mx>
2024-09-27 10:41:33 -0600

    Practica 09
    Modificacion a la practica 09

--- DartPad/Ex07_Clases.dart
+++ DartPad/Ex07_Clases.dart
@@ -10,7 +10,7 @@ void main()
   print(wolverine.power);
 
   final scarletWitch=Hero("Magia del Caos","Wanda");
-  print("Hero 1:--------------------------------------------------------------");
+  print("--------------------------------------------------------------");
   print(scarletWitch);
   print(scarletWitch.name);
   print(scarletWitch.power);
@@ -20,6 +20,12 @@ void main()
   print(xmen.name);
   print(xmen.type);
 
+  final  brotherhood = Team(name: "The Brotherhood of Evil Mutants", type: "Villanos");
+  print("--------------------------------------");
+    print(brotherhood.name);
+  print(brotherhood.type);
+  print(brotherhood.toString());
+  
   final justiceLigue = Team(name:"La liga de la justicia");
   print(justiceLigue);
   print(justiceLigue.name);
@@ -27,7 +33,7 @@ void main()
 
   //Intentamos instanciar la clase con solo el valor del tipo que no lo permite por que la 
   //final hydra = Team(type: "Villanos"); 
-  
+
 }
 //Valores Posicionales
 
@@ -50,4 +56,9 @@ class Team
 
   //El constructor de la clase siempre debe llevar el mismo nombre
   Team({required this.name, this.type="No definido."});
+
+  @override
+  String toString(){
+    return 'Grupo: $name, Tipo: $type';
+  }
 }
