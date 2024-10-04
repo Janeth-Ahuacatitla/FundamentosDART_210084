7a68006bd3c87256d63342261a71af0cf8a2fa11
Janeth <210084@utxicotepec.edu.mx>
2024-10-03 21:10:07 -0600

    Practica 10
    Modificacion de la practica

--- DartPad/Ex08_Constructores_Nombrados.dart
+++ DartPad/Ex08_Constructores_Nombrados.dart
@@ -1,9 +1,7 @@
-import 'dart:io';
-
 void main()
 {
   //Contructores nombrados 
-  //CASO DE PRUEBA 2
+  //CASO DE PRUEBA 1
   final janethAmixtlan = Person(
       ID: 250,
       name: "Janeth",
@@ -13,27 +11,26 @@ void main()
     bloodGroup: "O+",
     curp: "AUAJ031207MPLHMNA6",
     birthdate: DateTime(2003,12,07),
+    photoURL: "images/clients/250.png",
     isActive: true,
     createdAt: DateTime(2024,09,2024,11,05,16),
   );
 
   print(janethAmixtlan);
   
-  //Caso de prueba 2
+//CASO DE PRUEBA 2 -- Enviando solo los Datos requeridos (obligatorios)
   final marthaFlores = Person(
-    ID: 125,
-    name: "Martha",
-    firstLastname: "Flores",
-    gender: "Mujer",
-    bloodGroup: "O-",
-    birthdate:  DateTime(2008,01,08));
+      ID: 125,
+      name: "Martha",
+      firstLastname: "Flores",
+      gender: "Mujer",
+      bloodGroup: "O-",
+      birthdate: DateTime(2008, 01, 08));
 
   print(marthaFlores);
 }
 
-
-  class Person
-    {
+    class Person {
       int ID;
       String? cortesyTitle;
       String name;
@@ -41,65 +38,63 @@ void main()
       String? secondLastname;
       String gender;
       String bloodGroup;
+      String? photoURL;
       String? curp;
       DateTime birthdate;
       bool isActive;
       DateTime createdAt;
       DateTime? updatedAt;
 
-  Person(
-    {
-      required this.ID,
-      required this.name,
-      required this.firstLastname,
-      this.secondLastname,
-      required this.gender,
-      required this.bloodGroup,
-      this.curp,
-      required this.birthdate,
-      this.isActive = true,
-      DateTime? createdAt,
-      this.updatedAt
-
-    }
-  ) : createdAt = createdAt ??  DateTime.now();
-
+      Person(
+          {required this.ID,
+          this.cortesyTitle,
+          required this.name,
+          required this.firstLastname,
+          this.secondLastname,
+          required this.gender,
+          required this.bloodGroup,
+          this.photoURL,
+          this.curp,
+          required this.birthdate,
+          this.isActive = true,
+          DateTime? createdAt,
+          this.updatedAt})
+          : createdAt = createdAt ?? DateTime.now();
 
 @override
-String toString()
-{
-  String formatedBirthDate =   "${birthdate.day.toString().padLeft(2,'0')}/"+
-    "${birthdate.month.toString().padLeft(2,'0')}/${birthdate.year}";
+String toString() {
+  final String formatedBirthDate =
+      "${birthdate.day.toString().padLeft(2, '0')}/" +
+          "${birthdate.month.toString().padLeft(2, '0')}/${birthdate.year}";
 
-  String formatedCreateDate =   "${createdAt.day.toString().padLeft(2,'0')}/" +
-    "${createdAt.month.toString().padLeft(2,'0')}/${createdAt.year}
-  ${createdAt.hour.toString().padLeft(2,'0')}:${createdAt.minute.toString().padLeft(2,'0')}:
-    ${createdAt.second.toString().padLeft(2,'0')}";
+  final String formatedCreatedDate =
+      "${createdAt.day.toString().padLeft(2, '0')}/" +
+          "${createdAt.month.toString().padLeft(2, '0')}/${createdAt.year} ${createdAt.hour.toString().padLeft(2, '0')}:${createdAt.minute.toString().padLeft(2, '0')}:${createdAt.second.toString().padLeft(2, '0')}";
 
-  curp??= "No registrado";
-  if(curp == "") curp = "No registrado";
-  
-  final String status = "Sin definir";
-  
-  if(isActive)
+  curp ??= "No registrado";
+  if (curp == "") curp = "No registrado";
+
+  String status = "Sin definir";
+  if (isActive)
     status = "Activo";
   else
     status = "Inactivo";
-  
-    
+
   return """
-  -------------------------------------------------------
-  DATOS DE LA PERSONA
-  ---------------------------------------------
-  ID: $ID,
-  Nombre: $name $firstLastname $secondLastname
-  Género: $gender
-  Grupo Sanguíneo: $bloodGroup
-  Fecha de Nacimiento: $birthdate
-  CURP: $curp
-  Activo: $isActive
-  Fecha de Registro:  $formatedCreateDate
-  -------------------------
-  """;
- }
+
+----------------------------------------------------------
+   DATOS DE LA PERSONA
+   ----------------------------------------------------------
+   ID: $ID,
+   Nombre: $name $firstLastname $secondLastname
+   Género: $gender
+   Grupo Sanguíneo: $bloodGroup
+   Fecha de Nacimiento: $formatedBirthDate
+   Foto: $photoURL
+   CURP:  $curp
+   Estatus: $status
+   Fecha de Registro: $formatedCreatedDate
+   ----------------------------------------------------------
+   """;
+  }
 }
