3c711159da08287f8b386173f84baae7c7539e0f
Janeth <210084@utxicotepec.edu.mx>
2024-10-07 20:26:03 -0600

    Modificacion archivo

--- DartPad/Ex10_Clases_Abstractas.dart
+++ DartPad/Ex10_Clases_Abstractas.dart
@@ -1,49 +1,54 @@
 void main() {
-  final windPlant = WindPlant(initialEnergy: 100);
-  final nuclearPlant = NuclearPlant(energyLeft: 1000);
-
-  print('wind: ${chargePhone(windPlant)}');
-  print('nuclear: ${chargePhone(nuclearPlant)}');
+  final windPlant = WindPlant( initialEnergy: 100 );
+  final nuclearPlant = NuclearPlant( energyLeft: 1000 );
+  
+  
+  print( 'wind: ${ chargePhone( windPlant ) }' );
+  print( 'nuclear: ${ chargePhone( nuclearPlant ) }' );
 }
-
-double chargePhone(EnergyPlant plant) {
-  if (plant.energyLeft < 10) {
+double chargePhone( EnergyPlant plant ) {
+  if ( plant.energyLeft < 10 ) {
     throw Exception('Not enough energy');
   }
-  plant.consumeEnergy(10); // Consume la energía antes de devolver el resultado
-  return plant.energyLeft; // Devuelve la energía restante después de cargar
+    
+  return plant.energyLeft - 10;
 }
-
 enum PlantType { nuclear, wind, water }
-
 abstract class EnergyPlant {
+  
   double energyLeft;
   final PlantType type; // nuclear, wind, water
-
-  EnergyPlant({
-    required this.energyLeft,
-    required this.type,
+  
+  EnergyPlant({ 
+    required this.energyLeft, 
+    required this.type 
   });
-
-  void consumeEnergy(double amount);
+  
+  void consumeEnergy( double amount );
+  
 }
-
 class WindPlant extends EnergyPlant {
-  WindPlant({required double initialEnergy})
-      : super(energyLeft: initialEnergy, type: PlantType.wind);
-
+  
+  WindPlant({ required double initialEnergy })
+      : super( energyLeft: initialEnergy, type: PlantType.wind );
+  
   @override
-  void consumeEnergy(double amount) {
+  void consumeEnergy( double amount ) {
     energyLeft -= amount;
   }
 }
-
-class NuclearPlant extends EnergyPlant { // Cambié a herencia
-  NuclearPlant({required double energyLeft})
-      : super(energyLeft: energyLeft, type: PlantType.nuclear);
-
+class NuclearPlant implements EnergyPlant {
+  
   @override
-  void consumeEnergy(double amount) {
-    energyLeft -= (amount * 0.5); // Asegúrate de usar -= aquí
+  double energyLeft;
+  
+  @override
+  final PlantType type = PlantType.nuclear;
+  
+  NuclearPlant({ required this.energyLeft });
+  
+  @override
+  void consumeEnergy( double amount ) {
+    energyLeft -= (amount * 0.5);
   }
 }
